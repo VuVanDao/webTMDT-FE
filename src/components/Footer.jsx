8d3440b51f3121106499f3e37f6ba04d69eff7c5
Van Dao <dao28901@gmail.com>
2025-04-20 21:10:50 +0700

    Xong phan footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,5 @@
-import { Box, Container } from "@mui/material";
+import { Box, Container, Stack, Typography } from "@mui/material";
 import React from "react";
-import { theme } from "../theme";
 
 const Footer = () => {
   return (
@@ -11,7 +10,37 @@ const Footer = () => {
         color: "black",
       }}
     >
-      <Container> Footer</Container>
+      <Container>
+        <Box sx={{ p: 2, textAlign: "center" }}>
+          <Box
+            sx={{ display: "flex", gap: 2, justifyContent: "center", mb: 2 }}
+          >
+            <Box>Chính sách bảo mật</Box>
+            <Box>Quy chế bảo mật</Box>
+            <Box>Chính sách vận chuyển</Box>
+            <Box>Chính sách trả hàng và hoàn tiền</Box>
+          </Box>
+          <Typography sx={{ fontSize: "12px", mb: 3 }}>
+            Công ty TNHH Shopee
+          </Typography>
+          <Typography sx={{ fontSize: "12px", mb: 3 }}>
+            Địa chỉ: Tầng 4-5-6, Tòa nhà Capital Place, số 29 đường Liễu Giai,
+            Phường Ngọc Khánh, Quận Ba Đình, Thành phố Hà Nội, Việt Nam. Chăm
+            sóc khách hàng: Gọi tổng đài Shopee (miễn phí) hoặc Trò chuyện với
+            Shopee ngay trên Trung tâm trợ giúp
+          </Typography>
+          <Typography sx={{ fontSize: "12px", mb: 3 }}>
+            Chịu Trách Nhiệm Quản Lý Nội Dung: Vũ Văn Đạo
+          </Typography>
+          <Typography sx={{ fontSize: "12px", mb: 1 }}>
+            Mã số doanh nghiệp: 0106773786 do Sở Kế hoạch và Đầu tư TP Hà Nội
+            cấp lần đầu ngày 10/02/2015
+          </Typography>
+          <Typography sx={{ fontSize: "12px" }}>
+            © 2015 - Bản quyền thuộc về Công ty TNHH Shopee
+          </Typography>
+        </Box>
+      </Container>
     </Box>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ import {
   Divider,
   TextField,
   Tooltip,
+  Autocomplete,
   Typography,
 } from "@mui/material";
 import FacebookIcon from "@mui/icons-material/Facebook";
@@ -15,23 +16,98 @@ import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 import LanguageIcon from "@mui/icons-material/Language";
 import LanguageChange from "./Language/Language";
 import shopeeImg from "../assets/shopee.png";
-import SearchIcon from "@mui/icons-material/Search";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import QrShope from "./QRSHOPEE/QrShope";
 import CartItem from "./Cart/CartItem";
 import MyAccount from "./MyAccount/MyAccount";
-const Header = () => {
+import { Link } from "react-router-dom";
+import { RecommendData } from "../Data/RecommenData";
+import SearchIcon from "@mui/icons-material/Search";
+const Header = ({ showHeader }) => {
   const [anchorEl, setAnchorEl] = useState(null);
 
   const [searchValue, setSearchValue] = useState("");
-
   const handlePopoverOpen = (event) => {
     setAnchorEl(event.currentTarget);
   };
   const handlePopoverClose = () => {
     setAnchorEl(null);
   };
-
+  if (!showHeader)
+    return (
+      <Box
+        sx={{
+          // height: (theme) => theme.customHeight.Header,
+          width: "100%",
+          bgcolor: (theme) => theme.commonColors,
+          color: "white",
+          p: 0.25,
+        }}
+      >
+        <Container maxWidth="lg">
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              px: 1,
+            }}
+          >
+            <Box>
+              <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
+                <Box>
+                  <Typography
+                    aria-owns={open ? "mouse-over-popover" : undefined}
+                    aria-haspopup="true"
+                    onMouseEnter={handlePopoverOpen}
+                    onMouseLeave={handlePopoverClose}
+                    sx={{ cursor: "pointer" }}
+                  >
+                    Tải ứng dụng
+                  </Typography>
+                  <QrShope anchorEl={anchorEl} setAnchorEl={setAnchorEl} />
+                </Box>
+                <Divider
+                  orientation="vertical"
+                  variant="middle"
+                  sx={{ bgcolor: "white", height: "15px" }}
+                />
+                <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
+                  <Typography>Kết nối</Typography>
+                  <Tooltip title="facebook">
+                    <FacebookIcon />
+                  </Tooltip>
+                  <Tooltip title="Instagram">
+                    <InstagramIcon />
+                  </Tooltip>
+                </Box>
+              </Box>
+            </Box>
+            <Box>
+              <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
+                <Box sx={{ display: "flex", gap: 0.5, alignItems: "center" }}>
+                  <NotificationsNoneIcon />
+                  <Typography>Thông báo</Typography>
+                </Box>
+                <Box sx={{ display: "flex", gap: 0.5, alignItems: "center" }}>
+                  <HelpOutlineIcon />
+                  <Typography>Hỗ trợ</Typography>
+                </Box>
+                <Box sx={{ display: "flex", gap: 0.5, alignItems: "center" }}>
+                  <LanguageIcon />
+                  <LanguageChange />
+                </Box>
+                <Box sx={{ display: "flex", gap: 0.5, alignItems: "center" }}>
+                  {/* <Avatar sx={{ width: 24, height: 24 }} />
+                <Typography>VuVanDao</Typography> */}
+                  <MyAccount />
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+        </Container>
+      </Box>
+    );
   return (
     <Box
       sx={{
@@ -111,44 +187,50 @@ const Header = () => {
             alignItems: "center",
           }}
         >
-          <Box>
+          <Box component={Link} to="/homePage">
             <img
               src={shopeeImg}
               style={{ width: "150px", cursor: "pointer" }}
             />
           </Box>
           <Box>
-            <TextField
-              id="outlined-search"
-              // label="Search ..."
-              type="search"
-              size="small"
-              onChange={(e) => setSearchValue(e.target.value)}
-              onKeyDown={(e) => {
-                if (e.keyCode === 13) alert(searchValue);
-              }}
-              InputProps={{
-                startAdornment: (
-                  <SearchIcon
-                    sx={{ cursor: "pointer" }}
-                    onClick={() => alert(searchValue)}
-                  />
-                ),
-              }}
-              sx={{
-                width: {
-                  md: "700px",
-                  sm: "400px",
-                },
-                "& .MuiOutlinedInput-root": {
-                  "&:hover fieldset": {
-                    borderColor: (theme) => theme.commonColor,
-                  },
-                  "&.Mui-focused fieldset": {
-                    borderColor: "white",
-                  },
-                },
-              }}
+            <Autocomplete
+              freeSolo
+              id="free-solo-2-demo"
+              disableClearable
+              options={RecommendData.map((option) => option.name)}
+              renderInput={(params) => (
+                <TextField
+                  id="outlined-search"
+                  {...params}
+                  type="search"
+                  size="small"
+                  // onChange={(e) => handleSearch(e)}
+                  slotProps={{
+                    input: {
+                      ...params.InputProps,
+                      type: "search",
+                    },
+                  }}
+                  InputProps={{
+                    startAdornment: <SearchIcon sx={{ cursor: "pointer" }} />,
+                  }}
+                  sx={{
+                    width: {
+                      md: "700px",
+                      sm: "400px",
+                    },
+                    "& .MuiOutlinedInput-root": {
+                      "&:hover fieldset": {
+                        borderColor: (theme) => theme.commonColor,
+                      },
+                      "&.Mui-focused fieldset": {
+                        borderColor: "white",
+                      },
+                    },
+                  }}
+                />
+              )}
             />
           </Box>
           <Box>
